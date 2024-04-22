4373ac99f499c8ae158f356d607f97f09849f232
madegh <167677067+madegh@users.noreply.github.com>
2024-04-22 19:02:36 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,5 @@
 # limitations under the License.
 #
 
-#add_lunch_combo omni_P683S10-userdebug
-#add_lunch_combo omni_P683S10-eng
+add_lunch_combo omni_P683S10-userdebug
+add_lunch_combo omni_P683S10-eng
